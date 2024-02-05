36ebbdbe87020d15914bdc755266e27ab4e12fcd
joshuang321 <joshuang21978@gmail.com>
2024-02-06 07:43:15 +0800

    Added ApprovePass. Changed function signatures and added monthly pass amount limit to ApplicationCollection.cs

--- SEAsg1/ApplicationCollection.cs
+++ SEAsg1/ApplicationCollection.cs
@@ -2,13 +2,37 @@
 {
     public class ApplicationCollection : Collectable
     {
+        int monthlyPassesLeft;
         List<Application> apps = new List<Application>();
 
-        public Iterator CreateIterator(string type)
+        public ApplicationCollection(int amtOfMonthlyPasses)
         {
-            return new ApplicationIterator(this, type);
+            monthlyPassesLeft = amtOfMonthlyPasses;
         }
 
+        public Iterator CreateIterator()
+        {
+            return new ApplicationIterator(this);
+        }
+
+        public bool HasMonthlyLeft() => monthlyPassesLeft > 0;
+
+        public void ApprovePass(Application app)
+        {
+            if (app.GetPassType() == "Monthly" && monthlyPassesLeft<=0)
+            {
+                Console.WriteLine("Failed to approve monthly pass. Limit reached.");
+            }
+            float charge = app.GetCharge();
+            Console.WriteLine($"Approved {app.GetPassType()} Season Parking for {charge}.");
+            SeasonParking pass =new SeasonParking(app.GetStartMonth(), app.GetEndMonth(),
+                app.GetVehicle(),
+                app.GetChargeStrategy());
+
+            app.GetUser().AddPass(pass);
+            Remove(app);
+        }
+        
         public object? Get(int index)
         {
             if (index < apps.Count)
@@ -18,14 +42,14 @@
             return null;
         }
 
-        public void Remove(int index)
+        public void Remove(object app)
         {
-            apps.RemoveAt(index);
+            apps.Remove((Application)app);
         }
 
-        public void Add(Application app)
+        public void Add(object app)
         {
-            apps.Add(app);
+            apps.Add((Application)app);
         }
     }
 }
